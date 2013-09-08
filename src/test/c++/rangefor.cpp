b75f70d2a3e16acdf9b1d01b5821adbb57c2728b
Timothy Schumacher, Ph.D <schumact@gmail.com>
2013-09-08 11:10:44 -0600

    Added a file to demonstrate the range based for loops in C++11.
    The loops included are
    (1) a standard c-style array.
    (2) a iterable class with begin() and end() style interface (stl containers)
    (3) an initializer list style {...} loop.

---
+++ src/test/c++/rangefor.cpp
@@ -0,0 +1,29 @@
+/*
+ *  <p>Simple demonstraion of range based for loops from C++11</p>
+ *
+ */
+#include<iostream>
+#include<vector>
+#include<string>
+
+int main(int argc, char* argv[]) {
+
+    // Simple c-style array iteration:
+    int arr[] = {1,2,3,4,5};
+    for(int& x : arr) {
+        std::cout << "c-style array loop: x=" << x << std::endl;    
+    }
+
+    // begin()/end() iterator style loop:
+    std::vector<std::string> vec = { "foo", "bar", "baz" };
+    for(std::string& s : vec) {
+        std::cout << "begin()/end() iterator loop: s=" << s << std::endl;    
+    }
+
+    // initializer list verion:
+    for(double d : {3.14, 3.141, 3.1415, 3.14159}) {
+        std::cout << "initializer list loop: d=" << d << std::endl;    
+    }
+
+    return 0;
+}
