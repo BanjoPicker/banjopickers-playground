440e4aec408371568a6d625bcb37844a3a6b67c6
Timothy Schumacher, Ph.D <schumact@gmail.com>
2013-09-15 16:25:22 -0600

    Playing with linear regression functions and expirementing with creating plots.

---
+++ src/test/r/linearregression.R
@@ -0,0 +1,36 @@
+#!/usr/bin/rscript
+
+# Load the data library MASS
+library("MASS")
+
+# attach the Boston database so variable names can be resolved without having to specify them:
+attach(Boston)
+
+# Show the names of the columns:
+names(Boston)
+
+# The form for a linear regression is y~x which means y = a+bx 
+# We can also do things like y~ x + I(x^2) to fit a quadratic funtion
+lm.fit1 = lm(medv ~ poly(lstat,1), data = Boston)
+lm.fit2 = lm(medv ~ poly(lstat,2), data = Boston)
+lm.fit3 = lm(medv ~ poly(lstat,3), data = Boston)
+
+print("Basic fit information:")
+lm.fit1
+
+print("More detailed fit information:")
+print( summary( lm.fit1 ) )
+
+# Look at what attributes are available in lm.fit:
+print("Attributes in the fit variable:")
+names(lm.fit1)
+
+# Summary:
+summary(lm.fit1)$sigma
+
+# ANOVA table to compare models:
+anova(lm.fit1, lm.fit2, lm.fit3)
+
+plot(lstat,medv)
+plot(lm.fit2)
+abline(lm.fit1)
