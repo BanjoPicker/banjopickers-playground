ef79c92b92c7380305f47eda6c2a9d2c5525d0fe
Timmy Schumacher, Ph.D <schumact@colorado.EDU>
2014-04-15 21:06:15 -0700

    to_string play test.

--- src/test/c++/to_string.cc
+++ src/test/c++/to_string.cc
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 
+// Simple demonstration of the std::to_string functionality
+// introduced in the c++11 standard.
+
 int main(int argc, char* argv[]) {
     std::cout << std::to_string(67) << std::endl;
     return 0;
