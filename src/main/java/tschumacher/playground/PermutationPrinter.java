2ea7f0a878547ef77d346f13476d02799382be13
Timothy Schumacher, Ph.D <schumact@gmail.com>
2014-08-30 11:16:17 -0700

    Added and int[] permutation printer.

--- src/main/java/tschumacher/playground/PermutationPrinter.java
+++ src/main/java/tschumacher/playground/PermutationPrinter.java
@@ -1,5 +1,7 @@
 package tschumacher.playground;
 
+import java.util.Arrays;
+
 /**
  * 	<p>
  * 		Problem is to write a program to print out all permutations 
@@ -15,20 +17,65 @@ package tschumacher.playground;
 
 public class PermutationPrinter {
 
-    private static void PrintPermutations(String acc, String s) {
-        if(s.length() <= 1) {
-            System.out.println(acc + s);
-        } else {
-            for(int i=0;i<s.length();i++) {
-                String c = s.substring(i,i+1);
-                String t = s.substring(0,i) + s.substring(i+1);
-                PrintPermutations(acc + c, t);
-            }
-        }
-    }
+  /**
+   * <p>Print out all permutations of the string s.</p>
+   * <p>Sample usage: <br/>
+   *    PrintPermutations("", "hello world!");
+   * </p>
+   */
+  private static void PrintPermutations(String acc, String s) {
+    if(s.length() <= 1) {
+      System.out.println(acc + s);
+    } else {
+      for(int i=0;i<s.length();i++) {
+        String c = s.substring(i,i+1);
+        String t = s.substring(0,i) + s.substring(i+1);
+        PrintPermutations(acc + c, t);
+      }
+   }
+ }
 
-    public static void main(String args[]) {
-        String word = args[0];
-        PrintPermutations("", word);       
+  /**
+   * <p>Helper to swap postiion i with position j in the array.
+   */
+  private static void swap(int[] arr, int i, int j) {
+    int tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+  }
+
+  /**
+   * <p>Print all permutations of the sub-array contained in [a,b).</p>
+   */
+  public static void PrintPermutations(int[] arr, int a, int b) {
+    if (a >= b) {
+      System.out.println(Arrays.toString(arr));
+      return;
     }
+    for (int i = a; i < b; ++i) {
+      swap(arr, a, i);
+      PrintPermutations(arr, a+1, b);
+      swap(arr, i, a);
+    }
+  }
+
+  /**
+   * <p>Print all permutations of the array arr.</p>
+   * 
+   * <p>
+   *   The array will be modified in place, but will be restored upon exit.
+   * </p>
+   */
+  public static void PrintPermutations(int[] arr) {
+    PrintPermutations(arr, 0, arr.length);
+  }
+
+  /**
+   * <p>Simple main to test things out.</p>
+   */
+  public static void main(String args[]) {
+    int[] testarr = {1,7,2,3};
+    PrintPermutations(testarr);
+    System.out.println(Arrays.toString(testarr));
+  }
 };
